8cad15d5b36787096c5fb99890a39daf29b4c5b6
pawan prasad <113040973+pawanprasad2@users.noreply.github.com>
2025-07-11 05:37:48 +0530

    Create Timeout Cancellation.js

---
+++ Timeout Cancellation.js
@@ -0,0 +1,21 @@
+
+// Given a function fn, an array of arguments args, and a timeout t in milliseconds, return a cancel function cancelFn.
+
+// After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+// setTimeout(cancelFn, cancelTimeMs)
+// Initially, the execution of the function fn should be delayed by t milliseconds.
+
+// If, before the delay of t milliseconds, the function cancelFn is invoked, it should cancel the delayed execution of fn. Otherwise, if cancelFn is not invoked within the specified delay t, fn should be executed with the provided args as arguments.
+
+
+const cancellable = function(fn, args, t) {
+    // cancelFn function//
+    const cancelFn = function (){
+      clearTimeout(timer);
+  };
+  const timer = setTimeout(()=>{
+      fn(...args)
+  }, t);
+  return cancelFn ;
+};
